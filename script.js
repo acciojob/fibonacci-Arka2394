c9357bf378f2b8772f34eb2bc9cd9467ab9efdeb
Arka2394 <87031423+Arka2394@users.noreply.github.com>
2023-04-19 13:38:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function fibonacci(num) {
-// your code here
+  // Base case: return 0 for num=0 and 1 for num=1
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  }
+  
+  // Recursive case: sum of two previous Fibonacci numbers
+  return fibonacci(num - 1) + fibonacci(num - 2);
 }
-
-module.exports = fibonacci;
