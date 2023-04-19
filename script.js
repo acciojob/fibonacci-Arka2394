1fe0dcd793613728db8b69654108955885ffd173
Arka2394 <87031423+Arka2394@users.noreply.github.com>
2023-04-19 13:40:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 function fibonacci(num) {
-  // Base case: return 0 for num=0 and 1 for num=1
-  if (num === 0) {
-    return 0;
-  } else if (num === 1) {
-    return 1;
+  // Error handling: return null for negative numbers
+  if (num < 0) {
+    return null;
   }
   
-  // Recursive case: sum of two previous Fibonacci numbers
-  return fibonacci(num - 1) + fibonacci(num - 2);
+  // Memoization: store computed Fibonacci numbers in an array
+  const memo = [0, 1];
+  
+  // Recursive helper function to calculate Fibonacci number
+  function fib(n) {
+    // If already computed, return memoized value
+    if (memo[n] !== undefined) {
+      return memo[n];
+    }
+    
+    // Otherwise, calculate Fibonacci number and store in memo
+    memo[n] = fib(n - 1) + fib(n - 2);
+    return memo[n];
+  }
+  
+  // Call recursive helper function with initial value of num
+  return fib(num);
 }
+
+module.exports = fibonacci;
